1f590820c72c62b9dd267a3fbd7f49bf775974b5
Shivam Ray <shivamray603@gmail.com>
2026-07-19 23:56:51 +0530

    Wait for database connection before starting server

--- server/server.js
+++ server/server.js
@@ -7,8 +7,6 @@ import postRoutes from "./routes/postRoutes.js";
 
 dotenv.config();
 
-connectDB();
-
 const app = express();
 
 app.use(cors());
@@ -22,6 +20,12 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
